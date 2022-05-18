2cc2a94ff018445ce719d447f4b724bff1e32d6e
Phan Duong <duongrobben95@gmail.com>
2022-05-18 16:35:03 +0700

    bai1

---
+++ a.js
@@ -0,0 +1,13 @@
+
+//var cars=["honda","toyota","sh"];
+//cars.join("");
+//console.log(cars);
+//var cars=[1,2,3];
+// console.log(cars.concat(4,5,6));
+// console.log(cars);
+// Bài 1: yêu cầu người dùng nhập vào 1 giá trị (size). tạo ra 1 mảng có cỡ là size,
+//các giá trị trong mảng được người dùng nhập vào.
+// Hiển thị mảng này ra màn hình
+// Bài 2: yêu cầu người dùng nhập vào 1 giá trị (size). Tạo ra mảng có cõ là size
+ //gồm các giá trị được sinh ngẫu nhiên trong [20,50]
+// Hiển thị mảng này ra màn hình
---
+++ array.js
@@ -0,0 +1,5 @@
+var cho1={type:'a'};
+var cho2={type:'b'};
+var cho3={type:'c'};
+var dancho=["cho1","cho2","cho3"];
+console.log(dancho);
